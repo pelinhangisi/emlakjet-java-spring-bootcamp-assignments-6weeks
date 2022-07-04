d19c809ce37d7b22292e5605f428b07968550bfe
pelinhangisi <pelinhangisi@gmail.com>
2022-07-04 22:49:27 +0300

    first commit

---
+++ Yeni klasör/src/main/java/com/pelinhangisi/week6assignment/Week6AssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.pelinhangisi.week6assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week6AssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week6AssignmentApplication.class, args);
+    }
+
+}
